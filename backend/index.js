6630a2c08a64847568570a776785808e77b0ca86
Mukarram Nawaz <156927769+MukarramNawaz@users.noreply.github.com>
2024-08-03 08:16:06 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use (cors(
-    
-))
+app.use(cors({
+    origin: 'https://personal-account-manager-app-mern-wjef.vercel.app'
+}));
 app.use(bodyParser.json());
 app.use ("", appRoutes)
 
