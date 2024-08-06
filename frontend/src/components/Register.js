cb7a592463dbf89dab436553f9202f02fb414b22
Mukarram Nawaz <156927769+MukarramNawaz@users.noreply.github.com>
2024-08-06 18:55:27 +0500

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -10,7 +10,7 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch(`https://personal-account-manager-app-mern.vercel.app/register`, {
+    const response = await fetch('https://personal-account-manager-app-mern.vercel.app/register', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
